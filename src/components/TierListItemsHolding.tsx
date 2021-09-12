10d956592dc0dcbdfa6925fa1501f4205ed5ccb4
AnthonySorise <contact@anthonysorise.com>
2021-09-12 02:10:41 -0700

    list item holding styling

--- src/components/TierListItemsHolding.tsx
+++ src/components/TierListItemsHolding.tsx
@@ -1,17 +1,28 @@
 import {useContext, useEffect} from 'react';
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 import {Context} from '../App'
 import TierListItem from './TierListItem'
 import Box from '@material-ui/core/Box';
 import CircularProgress from '@material-ui/core/CircularProgress';
 
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        root: {
+            height: "calc(100vh - 680px)",
+            overflow: "auto"
+        }
+    }),
+);
 const TierListItemsHolding = () =>{
+    
     const {state, dispatch, episodeData, isEpisodeDataLoaded} = useContext(Context);
     useEffect(function(){
         console.log("loadedFromListItemHolding")
     }, [isEpisodeDataLoaded])
 
+    const classes = useStyles();
     return (
-        <Box component="div" m={1}>
+        <Box component="div" m={1} className={classes.root}>
             {
                 isEpisodeDataLoaded ? 
                     episodeData.map(function(episode:any, i:number){
