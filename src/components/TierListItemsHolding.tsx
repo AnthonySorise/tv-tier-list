98d1ecaf6aadb417c4994051b44ec43168b40701
AnthonySorise <contact@anthonysorise.com>
2021-09-12 01:58:51 -0700

    list items

--- src/components/TierListItemsHolding.tsx
+++ src/components/TierListItemsHolding.tsx
@@ -1,13 +1,32 @@
-import react from 'react';
+import {useContext, useEffect} from 'react';
+import {Context} from '../App'
+import TierListItem from './TierListItem'
 import Box from '@material-ui/core/Box';
-import Card from '@material-ui/core/Card';
-
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 const TierListItemsHolding = () =>{
+    const {state, dispatch, episodeData, isEpisodeDataLoaded} = useContext(Context);
+    useEffect(function(){
+        console.log("loadedFromListItemHolding")
+    }, [isEpisodeDataLoaded])
+
     return (
         <Box component="div" m={1}>
-            Tier List Items Holding
-
+            {
+                isEpisodeDataLoaded ? 
+                    episodeData.map(function(episode:any, i:number){
+                        return(
+                        <TierListItem
+                            season={episode.season} 
+                            episode={episode.number} 
+                            title={episode.name} 
+                            img={episode.image.medium} 
+                            description = {episode.summary}
+                            key={i}/>
+                        )
+                    })
+                :<CircularProgress />
+            }
         </Box>
     )
 }
--- src/components/TierListTierRow.tsx
+++ src/components/TierListTierRow.tsx
@@ -1,23 +1,22 @@
 import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 import Box from '@material-ui/core/Box';
 import Grid from '@material-ui/core/Grid';
-import Card from '@material-ui/core/Card';
 
 const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-        height: "100px",
-        display:"flex",
-        alignItems: "center",
-        margin:"0",
-    },
-    rowLabel: {
-        fontSize: "2em",
-    },
-    rowItem: {
-        textAlign: "left",
-    },
-  }),
+    createStyles({
+        root: {
+            height: "115px",
+            display:"flex",
+            alignItems: "center",
+            margin:"0",
+        },
+        rowLabel: {
+            fontSize: "2em",
+        },
+        rowItem: {
+            textAlign: "left",
+        },
+    }),
 );
 export interface TierListRow_PropsInterface {
     tierLabel: string,
@@ -26,13 +25,12 @@ export interface TierListRow_PropsInterface {
 const TierListTierRow = ({tierLabel, tierColor}:TierListRow_PropsInterface) =>{
     const classes = useStyles();
     return (
-
         <Box component="div" m={1} style={{backgroundColor: tierColor}} className={classes.root}>
             <Grid container spacing={2}>
-                <Grid item xs={3} className={classes.rowLabel}>
+                <Grid item xs={2} className={classes.rowLabel}>
                     {tierLabel}
                 </Grid>
-                <Grid item xs={9} className={classes.rowItem}>
+                <Grid item xs={10} className={classes.rowItem}>
 
                 </Grid>
 
